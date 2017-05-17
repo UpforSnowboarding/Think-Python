46f4f781a95d31247b71b6c3db622cacc6d941a5
iivan02 <ivan_giang@hotmail.com>
2017-05-17 23:53:47 +1000

    Update ex7.1.py

--- ch7/ex7.1.py
+++ ch7/ex7.1.py
@@ -1,23 +1,19 @@
-
-def sqrt(a):
-    epsilon = 0.00000000001
-    x = 10
-    while True:
-        y = (x + a/x) / 2
-        if abs(y - x) < epsilon:
-            return y
-            break
-        x = y
-
-sqrt(1)
+def mysqrt(a):
+	epsilon = 0.0001
+	x = 10.00000
+	while True:
+		y = (x + a/x) / 2
+		if abs(y - x) < epsilon:
+			return y
+			break
+		x = y
+		
 import math
 
-
 def test_square_root():
-    print("a          mysqrt(a)          math.sqrt(a)          diff")
-    print("-	      ---------	         ------------	       ----")
-    for a in range(1, 10):
-        print("%.1f       %.5f	             %.5f	               %f" % (a, sqrt(a), math.sqrt(a), sqrt(a) - math.sqrt(a)))
-
-
+	print ("a     mysqrt(a)     math.sqrt(a)     delta")
+	print ("-     ---------     ------------     -----")
+	for a in range(1, 10):
+		print ('{:.1f}    {:.5f}        {:.5f}       {:f}'.format(a,mysqrt(a), math.sqrt(a), mysqrt(a)-math.sqrt(a)))
+		
 test_square_root()
