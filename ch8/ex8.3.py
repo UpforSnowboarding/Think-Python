f7f063bafa603dbd3ce50abc0708ec60405f8d97
iivan02 <ivan_giang@hotmail.com>
2017-05-20 01:07:52 +1000

    Update ex8.3.py

--- ch8/ex8.3.py
+++ ch8/ex8.3.py
@@ -1,10 +1,18 @@
 # use the reverse string slice to complete the is_palindrome exercise
 
 def is_palindrome(word):
-    if word[:] == word[::-1]:
+    if word == word[::-1]:
         return True
     else:
         return False
 
 print(is_palindrome('banana'))
 print(is_palindrome('redivider'))
+
+# one lined version
+
+def is_palindrome2(word):
+    return word == word[::-1]
+
+print(is_palindrome2('banana'))
+print(is_palindrome2('redivider'))
