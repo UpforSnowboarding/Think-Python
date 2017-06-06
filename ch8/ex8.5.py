630555ed5627abbee4303bd6c80a58c9618b339c
iivan02 <ivan_giang@hotmail.com>
2017-06-06 23:41:59 +1000

    Update ex8.5.py

--- ch8/ex8.5.py
+++ ch8/ex8.5.py
@@ -28,7 +28,7 @@ def encrypt_letter(char,n):
 
     letter = ord(char) - begin
     enc_letter = (letter + n) % 26 + begin # the modulo % 26 instils the base case, ensures the evaluation doesn't exceed the upper
-    #bound of the key (no. of chars in the alphabet i.e. 26)
+    #bound of the key (no. of chars in the alphabet i.e. 26) because result of % 26 can't exceed val of 25
     return chr(enc_letter)
 
 print(encrypt_letter('B', 5))
